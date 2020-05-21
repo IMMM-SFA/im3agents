a6b63e8ff23fa6feaf798f86ebe2f608ad7d1496
crvernon <chrisrvernon@gmail.com>
2020-05-21 15:05:31 -0400

    add additional test

--- im3agents/tests/test_farmers.py
+++ im3agents/tests/test_farmers.py
@@ -18,6 +18,7 @@ class TestFarmers(unittest.TestCase):
 
         error_min_age = FarmerOne(age=-1)
         error_max_age = FarmerOne(age=151)
+        valid = FarmerOne(age=32)
 
         # expect value errors for exceeding min and max
         with self.assertRaises(ValueError):
@@ -25,6 +26,9 @@ class TestFarmers(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             error_max_age.age
+
+        # expect valid age
+        self.assertEqual(valid.age, 32)
 
 
 if __name__ == '__main__':
